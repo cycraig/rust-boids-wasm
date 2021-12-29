a24209e83c79dc13a033230d3b1e9c69e06a93d6
Craig Bester <craig.bester@iota.org>
2021-12-29 17:57:05 +0200

    Fix clippy lints

--- benches/bench_boids.rs
+++ benches/bench_boids.rs
@@ -1,8 +1,8 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use boids::BoidFlock;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::Rng;
 
-fn init_boid_flock(n : isize) -> BoidFlock {
+fn init_boid_flock(n: isize) -> BoidFlock {
     let mut rng = rand::thread_rng();
     let mut flock = BoidFlock::new(n as usize);
     let width: usize = 600;
@@ -12,20 +12,22 @@ fn init_boid_flock(n : isize) -> BoidFlock {
     let positions = flock.positions_mut();
     unsafe {
         for i in 0..n {
-            *positions.offset(2*i) = rng.gen_range(0..width) as f32;
-            *positions.offset(2*i+1) = rng.gen_range(0..height) as f32;
+            *positions.offset(2 * i) = rng.gen_range(0..width) as f32;
+            *positions.offset(2 * i + 1) = rng.gen_range(0..height) as f32;
         }
     }
     flock
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("flock", |b| b.iter(|| {
-        let mut flock = init_boid_flock(black_box(100));
-        for _ in 0..black_box(120) {
-            flock.update();
-        }
-    } ));
+    c.bench_function("flock", |b| {
+        b.iter(|| {
+            let mut flock = init_boid_flock(black_box(100));
+            for _ in 0..black_box(120) {
+                flock.update();
+            }
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
